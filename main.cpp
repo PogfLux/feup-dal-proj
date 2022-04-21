825e51ac4541ea5d284b669fcdd291294780a762
toni-santos <a.oliveirasantos122@gmail.com>
2022-04-21 23:31:12 +0100

    removed unused testing code

--- main.cpp
+++ main.cpp
@@ -15,24 +15,5 @@ int main() {
 		std::cout << "Shutting down..." << std::endl;
 	}
 
-    /*
-    // for testing purposes
-
-    std::cout << company.get_garage_size() << '\n';
-
-    for (int i = 0; i < 10; i++) {
-        std::cout << company.get_truck(i) << "\n";
-    }
-
-    std::cout << "\n\nReading deliveries\n\n";
-
-    std::cout << company.get_warehouse_size() << '\n';
-
-    for (int i = 0; i < 10; i++) {
-        std::cout << company.get_delivery(i) << '\n';
-    }
-
-    */
-
     return 0;
 }
